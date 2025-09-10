d1c9ae2753b414f748d659b4ce8be9f0243d3615
Jens Simon <jens.simon80@googlemail.com>
2025-09-10 23:07:18 +0200

    fix: migrate to new version

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,6 @@
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
 import { defineConfig } from 'eslint/config'
+import { configs } from '@jenssimon/eslint-config-base'
 import tseslint from 'typescript-eslint'
-
-import { FlatCompat } from '@eslint/eslintrc'
-import { fixupConfigRules } from '@eslint/compat'
-
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url) // eslint-disable-line no-underscore-dangle
-const __dirname = path.dirname(__filename) // eslint-disable-line no-underscore-dangle
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
 
 
 export default defineConfig(
@@ -26,12 +12,7 @@ export default defineConfig(
     ],
   },
 
-  ...fixupConfigRules(compat.config({
-    extends: [
-      '@jenssimon/base',
-      'plugin:import-esm/recommended',
-    ],
-  })),
+  configs.base,
 
   tseslint.configs.recommended,
   tseslint.configs.stylisticTypeChecked,
